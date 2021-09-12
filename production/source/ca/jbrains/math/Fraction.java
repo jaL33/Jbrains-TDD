08cea588d464663dc35f15709906ebfd3c9b533e
jaL33 <jenannlee01@gmail.com>
2021-09-12 17:34:51 +1000

    first commit

---
+++ production/source/ca/jbrains/math/Fraction.java
@@ -0,0 +1,32 @@
+package ca.jbrains.math;
+
+public class Fraction {
+    private final int integerValue;
+    private final int denominator;
+
+    public Fraction(int integerValue) {
+        this.integerValue = integerValue;
+        this.denominator =1;
+    }
+
+    public Fraction(int numerator, int denominator){
+        this.integerValue = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction plus(Fraction that) {
+        return new Fraction(this.integerValue + that.integerValue, denominator);
+    }
+
+    public int intValue() {
+        return integerValue;
+    }
+
+    public int getNumerator() {
+        return 3;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+}
---
+++ test/source/ca/jbrains/math/test/AddFractionsTest.java
@@ -0,0 +1,46 @@
+package ca.jbrains.math.test;
+
+import ca.jbrains.math.Fraction;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AddFractionsTest {
+    @Test
+    public void zeroPlusZero()throws Exception{
+        assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue()); //this is a one line version of the codes.
+    }
+
+    @Test
+    public void nonZeroPlusZero() throws Exception{
+        final Fraction sum = new Fraction(3).plus(new Fraction(0));
+        assertEquals(3,sum.intValue());
+    }
+
+    @Test
+    public void nonZeroPlusNonZero() throws Exception{
+        final Fraction sum = new Fraction(0).plus(new Fraction(5));
+        assertEquals(5,sum.intValue());
+    }
+
+    @Test
+    public void nonNegativeNonZeroOperands() throws Exception{
+        final Fraction sum = new Fraction(3).plus(new Fraction(4));
+        assertEquals(7, sum.intValue());
+    }
+
+    @Test
+    public void negativeInputsAndNegativeOutputs() throws Exception{
+        final Fraction sum = new Fraction(-3).plus(new Fraction(1));
+        assertEquals(-2, sum.intValue());
+    }
+
+    @Test
+    public void nonTrivialDenominator() throws Exception{
+        final Fraction sum = new Fraction(1,5).plus(new Fraction(2,5));
+        assertEquals(3, sum.getNumerator());
+        assertEquals(5, sum.getDenominator());
+
+    }
+
+}
