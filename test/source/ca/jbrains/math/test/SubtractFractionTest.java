c94f0a8cd7290a565574533c3e237be74e8b4fab
jaL33 <jenannlee01@gmail.com>
2021-09-17 17:05:19 +1000

    added subtractionTest ()

--- test/source/ca/jbrains/math/test/SubtractFractionTest.java
+++ test/source/ca/jbrains/math/test/SubtractFractionTest.java
@@ -11,5 +11,14 @@ public class SubtractFractionTest {
     public void zeroMinusZero()throws Exception{
         assertEquals(new Fraction(0), new Fraction(0).subtract(new Fraction(0)));
     }
+    @Test
+    public void nonZeroPlusNonZero() throws Exception{
+        assertEquals(new Fraction(4), new Fraction(5).subtract(new Fraction(1)));
+    }
+
+    @Test
+    public void negativeInputsAndNegativeOutputs() throws Exception{
+        assertEquals(new Fraction(-4), new Fraction(-3).subtract(new Fraction(1)));
+    }
 
 }
